2bbcb3a035e6be9ba30b101cdd5cdcf6f42d4dba
Khayyam Saleem <khayyamsaleemgmail.com@>
2017-01-29 15:24:31 -0500

    learned about user defined datatypes

--- hw2.rkt
+++ hw2.rkt
@@ -3,3 +3,14 @@
 ; I pledge my honor that I have abided by the Stevens Honor System.
 ;
 
+#lang racket
+
+(define-datatype dTree dTree?
+    (leaf-t
+      (datum number?))
+    (node-t
+      (key symbol?)
+      (left dTree?)
+      (right dTree?)))
+
+
