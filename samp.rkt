47302c7f6007dfd66eb3246843ebe34ab080b287
Khayyam Saleem <khayyam.saleem@gmail.com>
2017-04-21 12:46:35 -0400

    lecture stuff

---
+++ samp.rkt
@@ -0,0 +1,14 @@
+(run "
+begin
+type treeInt = 
+| Nil()
+| Node(int,treeInt,treeInt);
+let isEmpty=
+  proc(t:treeInt)
+    case t of {
+        <Nil>() -> 0,
+        <Node> -> <Nil>()
+        alkdjfal;kdfjal;kdf
+        asldf;kjadfl;
+    }
+  ")
