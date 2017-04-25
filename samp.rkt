b6ee31fdacdcbfadb058a570153b8b5ef4bd2662
Khayyam Saleem <khayyam.saleem@gmail.com>
2017-04-25 14:52:08 -0400

    Added stuff from lecture

--- samp.rkt
+++ samp.rkt
@@ -12,3 +12,21 @@ let isEmpty=
         asldf;kjadfl;
     }
   ")
+(run "
+module m1
+  interface
+   [u : int]
+  body
+   [u = 44]
+module m2
+  interface
+   [v : int]
+  body
+   [v = -(from m1 take u,11)]
+open m1
+in 
+open m2
+in
+let v=150
+in
+-(u,v)")
